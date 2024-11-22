a866c91e118abcbc1f2cf41954a05cf92d5a6dd7
kawooky <kawooky@gmail.com>
2024-11-22 12:29:35 +0000

    added imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client"; // Ensure ReactDOM is imported correctly
+import App from "./App"; // Ensure App is imported correctly
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
